cc2ab5dfe35153289404aaa66e025ff6a4388a86
Matheus Diógenes <mathdiogenes@gmail.com>
2023-07-13 14:37:48 -0300

    chores: Docker, npm dependencies, stylelint, eslint

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Chatbot from './pages/Chatbot';
+
+
+function App() {
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/chatbot" element={<Chatbot />} />
+                <Route path="*" element={<p>Path not resolved</p>} />
+            </Routes>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/pages/Chatbot.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../styles/Chatbot.module.css';
+
+function Chatbot() {
+    return (
+        <div className={styles['main-container']}>
+            <h1>Chatbot page</h1>
+        </div>
+    );
+}
+
+export default Chatbot;
