1fb03e53ffb92cdb60e8a328714d12c34e07e564
Matheus Diógenes <mathdiogenes@gmail.com>
2023-07-17 21:12:32 -0300

    delete: dist folder

--- app/backend/dist/src/middlewares/ErrorHandler.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandler = (err, req, res, _next) => {
-    const { statusCode, message } = err;
-    if (statusCode)
-        return res.status(err.statusCode).json({ message });
-    return res.status(500).json({ message: 'Internal Server Error' });
-};
-exports.default = errorHandler;
