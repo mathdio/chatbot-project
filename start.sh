b644b6da6a022370ca9641bc8dca9f565de82aad
Matheus Diógenes <mathdiogenes@gmail.com>
2023-08-31 14:35:46 -0300

    feat: start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+# List all files and folders in the current directory
+ls
+
 # Change directory to "frontend"
 cd frontend || exit 1
 
 # Run the "npm run start" command
 npm run start
-
-# Change back to the original directory
-cd - || exit 1
